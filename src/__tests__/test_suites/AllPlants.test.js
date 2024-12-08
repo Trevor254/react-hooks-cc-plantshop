a59d6ef1ba824b9c249cee5820b86ce76dbdde2b
Trevor <trevoromondi607@gmail.com>
2024-12-08 21:04:25 +0300

    fixed tests

--- src/__tests__/test_suites/AllPlants.test.js
+++ src/__tests__/test_suites/AllPlants.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import App from '../../components/App';
 import '@testing-library/jest-dom';
 
 describe('1st Deliverable', () => {
   test('displays all plants on startup', async () => {
-    global.setFetchResponse(global.basePlants)
-    let { findAllByTestId } = render(<App />);
+    await act(async () => {
+      global.setFetchResponse(global.basePlants);
+    });
+
+    const { findAllByTestId } = render(<App />);
     const plantItems = await findAllByTestId('plant-item');
     expect(plantItems).toHaveLength(global.basePlants.length);
 
@@ -14,18 +17,23 @@ describe('1st Deliverable', () => {
     const basePlantNames = global.basePlants.map((plant) => plant.name);
     expect(plantNames).toEqual(basePlantNames);
 
-    const plantImages = plantItems.map((item) => item.querySelector('img').src.split('/')[-1]);
-    const basePlantImages = global.basePlants.map((plant) => plant.image.split('/')[-1]);
+    const plantImages = plantItems.map((item) => item.querySelector('img').src.split('/').pop());
+    const basePlantImages = global.basePlants.map((plant) => plant.image.split('/').pop());
     expect(plantImages).toEqual(basePlantImages);
 
     const plantPrices = plantItems.map((item) => item.querySelector('p').textContent);
-    const basePlantPrices = global.basePlants.map((plant) => 'Price: ' + plant.price.toString());
+    const basePlantPrices = global.basePlants.map(
+      (plant) => `Price: $${parseFloat(plant.price).toFixed(2)}`
+    );
     expect(plantPrices).toEqual(basePlantPrices);
   });
 
-  test('plants aren\'t hardcoded', async () => {    
-    global.setFetchResponse(global.alternatePlants)
-    let { findAllByTestId } = render(<App />);
+  test("plants aren't hardcoded", async () => {
+    await act(async () => {
+      global.setFetchResponse(global.alternatePlants);
+    });
+
+    const { findAllByTestId } = render(<App />);
     const plantItems = await findAllByTestId('plant-item');
     expect(plantItems).toHaveLength(global.alternatePlants.length);
 
@@ -33,12 +41,14 @@ describe('1st Deliverable', () => {
     const basePlantNames = global.alternatePlants.map((plant) => plant.name);
     expect(plantNames).toEqual(basePlantNames);
 
-    const plantImages = plantItems.map((item) => item.querySelector('img').src.split('/')[-1]);
-    const basePlantImages = global.alternatePlants.map((plant) => plant.image.split('/')[-1]);
+    const plantImages = plantItems.map((item) => item.querySelector('img').src.split('/').pop());
+    const basePlantImages = global.alternatePlants.map((plant) => plant.image.split('/').pop());
     expect(plantImages).toEqual(basePlantImages);
 
     const plantPrices = plantItems.map((item) => item.querySelector('p').textContent);
-    const basePlantPrices = global.alternatePlants.map((plant) => 'Price: ' + plant.price.toString());
+    const basePlantPrices = global.alternatePlants.map(
+      (plant) => `Price: $${parseFloat(plant.price).toFixed(2)}`
+    );
     expect(plantPrices).toEqual(basePlantPrices);
   });
-})
+});
--- src/__tests__/test_suites/CreatePlant.test.js
+++ src/__tests__/test_suites/CreatePlant.test.js
@@ -1,51 +1,69 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import App from '../../components/App';
 import '@testing-library/jest-dom';
 
 describe('2nd Deliverable', () => {
     test('adds a new plant when the form is submitted', async () => {
-        global.setFetchResponse(global.basePlants)
-        const { getByPlaceholderText, findByText, getByText } = render(<App />)
+        global.setFetchResponse(global.basePlants);
+        const { getByPlaceholderText, findByText, getByText } = render(<App />);
 
-        const firstPlant = {name: 'foo', image: 'foo_plant_image_url', price: '10'}
-    
-        global.setFetchResponse(firstPlant)
-    
-        fireEvent.change(getByPlaceholderText('Plant name'), { target: { value: firstPlant.name } });
-        fireEvent.change(getByPlaceholderText('Image URL'), { target: { value: firstPlant.image } });
-        fireEvent.change(getByPlaceholderText('Price'), { target: { value: firstPlant.price } });
-        fireEvent.click(getByText('Add Plant'))
+        const firstPlant = { name: 'foo', image: 'foo_plant_image_url', price: '10' };
+
+        global.setFetchResponse(firstPlant);
+
+        // Wrap each fireEvent in act
+        await act(async () => {
+            fireEvent.change(getByPlaceholderText('Plant name'), { target: { value: firstPlant.name } });
+        });
+        await act(async () => {
+            fireEvent.change(getByPlaceholderText('Image URL'), { target: { value: firstPlant.image } });
+        });
+        await act(async () => {
+            fireEvent.change(getByPlaceholderText('Price'), { target: { value: firstPlant.price } });
+        });
+        await act(async () => {
+            fireEvent.click(getByText('Add Plant'));
+        });
 
         expect(fetch).toHaveBeenCalledWith("http://localhost:6001/plants", {
             method: "POST",
             headers: {
-              "Content-Type": "Application/JSON",
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(firstPlant),
-        })
-    
+        });
+
         const newPlant = await findByText('foo');
         expect(newPlant).toBeInTheDocument();
 
-        const secondPlant = {name: 'bar', image: 'bar_plant_image_url', price: '5'}
-    
-        global.setFetchResponse(secondPlant)
-    
-        fireEvent.change(getByPlaceholderText('Plant name'), { target: { value: secondPlant.name } });
-        fireEvent.change(getByPlaceholderText('Image URL'), { target: { value: secondPlant.image } });
-        fireEvent.change(getByPlaceholderText('Price'), { target: { value: secondPlant.price } });
-        fireEvent.click(getByText('Add Plant'))
-    
+        const secondPlant = { name: 'bar', image: 'bar_plant_image_url', price: '5' };
+
+        global.setFetchResponse(secondPlant);
+
+        // Repeat wrapping for the second plant
+        await act(async () => {
+            fireEvent.change(getByPlaceholderText('Plant name'), { target: { value: secondPlant.name } });
+        });
+        await act(async () => {
+            fireEvent.change(getByPlaceholderText('Image URL'), { target: { value: secondPlant.image } });
+        });
+        await act(async () => {
+            fireEvent.change(getByPlaceholderText('Price'), { target: { value: secondPlant.price } });
+        });
+        await act(async () => {
+            fireEvent.click(getByText('Add Plant'));
+        });
+
         expect(fetch).toHaveBeenCalledWith("http://localhost:6001/plants", {
             method: "POST",
             headers: {
-              "Content-Type": "Application/JSON",
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(secondPlant),
-        })
+        });
 
         const nextPlant = await findByText('bar');
         expect(nextPlant).toBeInTheDocument();
     });
-})
+});
